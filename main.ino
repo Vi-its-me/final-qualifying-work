21974e4d2ec530ea9f75cae5951e3bdfde0bffe6
Vi-its-me <vitalij.makarov_student@mail.ru>
2024-03-18 13:32:30 +0500

    18.03 it can sequently send array elements to another module and parralel receive data

--- main.ino
+++ main.ino
@@ -1,9 +1,12 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 #include "signal_amount_method.h" //#include "time_count_method.h"
-#define RECEIVE // MAC || AMOUNT || RECEIVE || TRANSMIT
+#define TRANSMIT // MAC || AMOUNT || RECEIVE || TRANSMIT
 
 #if defined RECEIVE || defined TRANSMIT
+  //Переменные для обеспечения распараллеливания в loop()
+  int interval = 1000;
+  int last_send_time = 0;
   // MAC-адреса плат
   uint8_t MAC_1[] = {0xC8, 0xC9, 0xA3, 0x5B, 0x96, 0x22};
   uint8_t MAC_2[] = {0xE8, 0xDB, 0x84, 0xBB, 0xE8, 0x3B};//Where to send-2
@@ -63,28 +66,38 @@ void loop()
   #ifdef TRANSMIT
     // Распараллеливание для отправки данных(отправляет масив единичек 
     // на 2 модуль)
-
-    // Инициализируем 1 модуль как отправляющий
-    esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
-    // Как только ESPNow инициализируется, мы зарегистрируем Send CB
-    // для получения статуса переданного пакета 
-    esp_now_register_send_cb(OnDataSent);
-    // Регистрируем пир
-    esp_now_add_peer(MAC_2, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
-    // Отправляем поочереди каждый из элементов массива на второй модуль
-    for(int i = 0; i < 10; i++)
+    if((millis() - last_send_time) >= interval)
     {
-      send_instance.to_second_point_packets_array[i] = 1;
-      // Засекаем время для каждого отправленного
-      send_instance.sent_time[i] = 0;
-      // Отправляем также номер пакета
-      send_instance.received_packets_number++;
-      esp_now_send(0, (uint8_t *) &send_instance, sizeof(send_instance));
+      // Инициализируем 1 модуль как отправляющий
+      esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+      // Как только ESPNow инициализируется, мы зарегистрируем Send CB
+      // для получения статуса переданного пакета 
+      esp_now_register_send_cb(OnDataSent);
+      // Регистрируем пир
+      esp_now_add_peer(MAC_2, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+      // Отправляем поочереди каждый из элементов массива на второй модуль
+      for(int i = 0; i < 10; i++)
+      {
+        if(send_instance.to_second_point_packets_array[i] == 0)
+          {
+            Serial.printf("send_instance.to_second_point_packets_array[%d] = 1\n", 
+        i);
+            send_instance.to_second_point_packets_array[i] = 1;
+            send_instance.sent_time[i] = 0;
+            // Увеличиваем количество отправленных единичек на 1
+            send_instance.received_packets_number++;
+            esp_now_send(0, (uint8_t *) &send_instance, sizeof(send_instance));
+            last_send_time = millis();
+            break;
+          }
+        // Засекаем время для каждого отправленного
+        
+      }
     }
-    //Распараллеливание через millis() для приема данных(если 0 меняется на 2,
-    // то фиксирует время и "выкидывает" в монитор данные об 
-    // изменившейся переменной)
-
+    // если 0 меняется на 2,то фиксирует время и "выкидывает" 
+    // в монитор данные об изменившейся переменной
+    esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+    esp_now_register_recv_cb(OnDataRecv);
   #endif // TRANSMIT
   #ifdef RECEIVE
     // Печатаем в порт Serial каждый элемент из массива и общую сумму принятых п-ов
