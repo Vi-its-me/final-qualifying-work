4cb29ca970b39ea9aafe577037fe1009740adffb
Vi-its-me <vitalij.makarov_student@mail.ru>
2024-04-24 17:01:07 +0500

    v2.1 added export of extracted part from Serial

--- serial_to_excel.py
+++ serial_to_excel.py
@@ -1,6 +1,6 @@
 import serial
 import time
-# from openpyxl import Workbook
+from openpyxl import Workbook
 print_condition_flag = False
 serial_object = serial.Serial()
 port_number = 5
@@ -15,22 +15,28 @@ while not serial_object.is_open:
         print(".", end="")
         time.sleep(1)
 print("\nOpened!")
-# workbook = Workbook()
+workbook = Workbook()
+worksheet = workbook.active
+worksheet_row = 1
 try:
     while True:
         serial_string = serial_object.readline().decode()
         if serial_string == "end\n":
-            break   # Переносом строки может стать ""\r\n"!
+            break  # Переносом строки может стать ""\r\n"!
+        worksheet_column = 1
         previous_iterator = 0
         iterator = 0
         if iterator:
             previous_iterator = iterator
         for char in serial_string:
             if char == "\t" or char == "\n":
-                print(serial_string[previous_iterator:iterator] + "\t|\t", end="")
+                worksheet.cell(row=worksheet_row, column=worksheet_column).value = serial_string[previous_iterator:iterator]
+                worksheet_column += 1
                 previous_iterator = iterator + 1
             iterator += 1
-        print(end="\n")
+        worksheet_row += 1
+        # перейти на другую строку
 except serial.SerialException:
     print(f"Nothing got from COM{port_number}")
+workbook.save(input("Enter the distance in meters: ") + "m" + ".xlsx")
 serial_object.close()
